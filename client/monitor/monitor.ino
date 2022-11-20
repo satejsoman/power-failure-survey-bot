feff03745f9b37b6e3b238230059f4595561125a
satej soman <satej.soman@gmail.com>
2022-11-20 14:33:28 -0800

    wifi connection skeleton

--- client/monitor/monitor.ino
+++ client/monitor/monitor.ino
@@ -1,14 +1,96 @@
+#include <WiFiNINA.h>
+
+#include "secrets.h" 
+char ssid[] = SECRET_SSID;    // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;             // your network key Index number (needed only for WEP)
+
+int indicator =  LED_BUILTIN;
+int sentinel = 12;
+int status = WL_IDLE_STATUS;
+
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(12, INPUT);
-  pinMode(13, OUTPUT);
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {} // wait for serial port to connect. Needed for native USB port only
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+
+  // set up on-board input and outpin pins
+  pinMode(sentinel, INPUT);
+  pinMode(indicator, OUTPUT);
+
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  while (digitalRead(12) == HIGH) {}
-  digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
+  while (digitalRead(sentinel) == HIGH) {}
+  Serial.println("Disconnect detected");
+  digitalWrite(indicator, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(500);              // wait for half a second
-  digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(indicator, LOW);   // turn the LED off by making the voltage LOW
   delay(500);              // wait for half a second
+}
+
+void printWifiData() {
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  printMacAddress(mac);
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
 }
