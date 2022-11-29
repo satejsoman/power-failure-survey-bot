664ef744225cb400b0575132e07ac2088dc14966
satej soman <satej.soman@gmail.com>
2022-11-28 18:32:47 -0800

    remove scaffolding code

--- client/ConnectWithWPA/arduino_secrets.h
+++
@@ -1,2 +0,0 @@
-#define SECRET_SSID "quabbledunk"
-#define SECRET_PASS "wompwomp"
--- client/WebserverAPMode/arduino_secrets.h
+++
@@ -1,5 +0,0 @@
-#define SECRET_SSID "quabbledunk"
-#define SECRET_PASS "wompwomp"
-
-#define WPA_SSID "devengc200-nline"
-#define WPA_PASS "huzzah1234"
